b8f72236393b2ba84777b249719ae30ba8b2d957
Winsten Coellins <62731781+winstencoellins@users.noreply.github.com>
2022-08-24 23:59:48 -0500

    Modify Stack.py

--- Linear Data Structures/assignment/Stack.py
+++ Linear Data Structures/assignment/Stack.py
@@ -9,46 +9,22 @@ class Stack:
 
     # Push a value into the stack
     def push(self, n):
-        if self.head is None:
-            self.head = Node(n)
-            return
-
-        newNode = Node(n)
-        newNode.next = self.head
-        self.head = newNode
+        pass
 
     # Pop the latest inserted value from the stack
     # If stack is empty, return any message
     def pop(self):
-        if self.head is None:
-            return "Can't pop. Stack is empty."
-
-        temp = self.head
-        del self.head
-        self.head = temp.next
-
-        return temp.val
+        pass
 
     # return the latest value of the stack
     def peek(self):
-        return self.head.val
+        pass
 
     # Check if stack is empty
     # return True if stack is empty, else False
     def isEmpty(self):
-        if self.head is not None:
-            return False
-        
-        return True
+        pass
 
     # Prints all of the value(s) in the stack
     def __str__(self):
-        curr = self.head
-        s = ""
-
-        while curr:
-            s += str(curr.val) + " "
-            curr = curr.next
-
-        return s
-
+        return
